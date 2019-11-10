1f080b7a0da666a51e9255bcb04ce6178d997abe
harideepreddy <harideepreddy2@gmail.com>
2019-11-11 02:16:19 +0530

    2 files modified

--- src/app/employeelist.component.ts
+++ src/app/employeelist.component.ts
@@ -8,17 +8,17 @@ export class EmployeeListComponent{
   employees: any[];
   constructor(){
 this.employees = [
-  {code:'emp1', name:'hari', dob:'14/02/1996'},
-  {code:'emp2', name:'harideep', dob:'14/03/1996'},
-  {code:'emp3', name:'haris', dob:'14/04/1996'}
+  {code:'emp1', name:'hari', dob:'04/02/1996',salary:'5000'},
+  {code:'emp2', name:'harideep', dob:'04/03/1996',salary:'5500'},
+  {code:'emp3', name:'haris', dob:'04/04/1996',salary:'5654'}
 ];
   }
 getEmployees(): void{
   this.employees = [
-{code:'emp1', name:'hari', dob:'14/02/1996'},
-  {code:'emp2', name:'harideep', dob:'14/03/1996'},
-  {code:'emp3', name:'haris', dob:'14/04/1996'},
-  {code:'emp4', name:'harish', dob:'14/05/1996'}
+{code:'emp1', name:'hari', dob:'04/02/1996',salary:'5000'},
+  {code:'emp2', name:'harideep', dob:'04/03/1996',salary:'5500'},
+  {code:'emp3', name:'haris', dob:'04/04/1996',salary:'5654'},
+  {code:'emp4', name:'harish', dob:'04/05/1996',salary:'6342'}
   ];
   }
   trackCode(index:number,employee:any): string{
